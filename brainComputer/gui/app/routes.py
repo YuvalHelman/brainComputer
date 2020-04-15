dec39412a1832a57a80109c585de4af0bc4dfb9e
Yuval Helman <yuvalasaf3@gmail.com>
2020-04-15 20:08:23 +0300

    working on API

--- brainComputer/gui/app/routes.py
+++ brainComputer/gui/app/routes.py
@@ -2,8 +2,10 @@ from brainComputer.gui.app import app
 from flask import render_template
 from collections import OrderedDict
 
+from brainComputer.gui.app.utils import pose_flatten, gui_image_dict_prepare
 
-@app.route('/users', methods=['GET'])
+@app.route('/', methods=['GET'])
+@app.route('/users/', methods=['GET'])
 def get_users_list():
     """ Returns the list of all the supported users, including their IDs and names only. """
     try:
@@ -17,7 +19,7 @@ def get_users_list():
         return "operation failed", 404
 
 
-@app.route('/users/<int:user_id>', methods=['GET'])
+@app.route('/users/<int:user_id>/', methods=['GET'])
 def get_user_id(user_id):
     """ Returns the specified user's details: ID, name, birthday and gender. """
     try:
@@ -27,7 +29,7 @@ def get_user_id(user_id):
         return "operation failed", 404
 
 
-@app.route('/users/<int:user_id>/snapshots', methods=['GET'])
+@app.route('/users/<int:user_id>/snapshots/', methods=['GET'])
 def get_user_snapshot_ids(user_id):
     """ Returns the list of the specified user's snapshot IDs and datetimes only. """
     try:
@@ -37,30 +39,34 @@ def get_user_snapshot_ids(user_id):
         return "operation failed", 404
 
 
-@app.route('/users/<int:user_id>/snapshots/<snapshot_id>', methods=['GET'])  # example: 1575446887339
+@app.route('/users/<int:user_id>/snapshots/<snapshot_id>/', methods=['GET'])  # example: 1575446887339
 def get_user_snapshot_details(user_id, snapshot_id):
     """ Returns the specified snapshot's details: ID, datetime, and the available results' names only (e.g. pose). """
     try:
         user_dict = app.config['db_handler'].get_user_id(user_id)
-      #  import pdb; pdb.set_trace()
         return render_template('snapshots_details.html', snapshots=user_dict['snapshots'], user=user_dict['user'], snapshot_id=snapshot_id, snapid_dict=user_dict['snapshots'][snapshot_id])
     except Exception as e:
         return "operation failed", 404
 
 
-@app.route('/users/<int:user_id>/snapshots/<snapshot_id>/<result_name>', methods=['GET'])
+@app.route('/users/<int:user_id>/snapshots/<snapshot_id>/<result_name>/', methods=['GET'])
 def get_snapshot_result(user_id, snapshot_id, result_name):
     """ Returns the specified snapshot's result. currently supports pose, color-image, depth-image and feelings.
         """
     try:
         user_dict = app.config['db_handler'].get_user_id(user_id)
-      #  import pdb; pdb.set_trace()
-        return render_template('snapshot_result.html', snapshots=user_dict['snapshots'], user=user_dict['user'], snapshot_id=snapshot_id, snapid_dict=user_dict['snapshots'][snapshot_id], res_dict=user_dict['snapshots'][snapshot_id][result_name] )
+        if result_name == 'pose':
+            res = pose_flatten(user_dict['snapshots'][snapshot_id][result_name])
+        elif result_name in ['color_image', 'depth_image']:
+            res = gui_image_dict_prepare(user_dict['snapshots'][snapshot_id][result_name], result_name)
+        else:
+            res = user_dict['snapshots'][snapshot_id][result_name]
+
+        return render_template('snapshot_result.html', snapshots=user_dict['snapshots'], user=user_dict['user'], snapshot_id=snapshot_id, snapid_dict=user_dict['snapshots'][snapshot_id], res_dict=res, res_name=result_name)
     except Exception as e:
         return "operation failed", 404
 
-
-@app.route('/users/<int:user_id>/snapshots/<snapshot_id>/<result_name>/data', methods=['GET'])
+@app.route('/users/<int:user_id>/snapshots/<snapshot_id>/<result_name>/data/', methods=['GET'])
 def get_snapshot_result_data(user_id, snapshot_id, result_name):
     """ Returns the specified snapshot's result image data. currently supports color-image, depth-image.
         """
---
+++ brainComputer/gui/app/utils.py
@@ -0,0 +1,15 @@
+import copy
+
+def pose_flatten(pose_dict):
+    res = dict()
+    for main_key, sub_dict in pose_dict.items():
+        for sub_key, sub_val in sub_dict.items():
+            res.update({main_key + '.' + sub_key : sub_val})
+    return res
+
+def gui_image_dict_prepare(image_dict, name):
+    import pdb; pdb.set_trace()
+    res = copy.deepcopy(image_dict)
+    res.pop('data_path')
+    res.pop(name+ '_path')
+    return res
